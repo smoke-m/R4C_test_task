7cedf0b8348f77ba69450a26fad3e61eeea437fc
Михаил Миноцкий <smoke-m@yandex.com>
2024-12-16 13:11:10 +0300

    day 1.01

--- robots/admin.py
+++ robots/admin.py
@@ -1,7 +1,16 @@
 from django.conf import settings
 from django.contrib import admin
 
-from .models import Robot
+from .models import Robot, RobotModel
+
+
+@admin.register(RobotModel)
+class RobotModelAdmin(admin.ModelAdmin):
+    """Админ зона Order."""
+
+    list_display = ("model_ind",)
+    empty_value_display = settings.EMPTY_VALUE
+    list_filter = ("model_ind",)
 
 
 @admin.register(Robot)
@@ -9,10 +18,10 @@ class RobotAdmin(admin.ModelAdmin):
     """Админ зона Order."""
 
     list_display = (
-        'serial',
-        'model',
-        'version',
-        'created',
+        "serial",
+        "model",
+        "version",
+        "created",
     )
     empty_value_display = settings.EMPTY_VALUE
-    list_filter = ('serial',)
+    list_filter = ("serial",)
--- robots/models.py
+++ robots/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 
-class Robot(models.Model):
+class RobotModel(models.Model):
+    model_ind = models.CharField(max_length=2, blank=False, null=False)
 
+    def __str__(self):
+        return self.model_ind
+
+
+class Robot(models.Model):
     serial = models.CharField(max_length=5, blank=False, null=False)
-    model = models.CharField(max_length=2, blank=False, null=False)
+    model = models.ForeignKey(
+        RobotModel,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
     version = models.CharField(max_length=2, blank=False, null=False)
     created = models.DateTimeField(blank=False, null=False)
+
+    def __str__(self):
+        return self.serial
