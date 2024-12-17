347dfe01578637c9320e6c8c0e1ff4c302099951
Михаил Миноцкий <smoke-m@yandex.com>
2024-12-17 19:30:42 +0300

    task_3 done

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import RobotViewSet
+from .views import OrderViewSet, RobotViewSet
 
 app_name = "api"
 
 router_v1 = routers.DefaultRouter()
+router_v1.register("orders", OrderViewSet, basename="orders")
 router_v1.register("robots", RobotViewSet, basename="robots")
 
 urlpatterns = [
